0663323fb5decb6601d7234cb6fc95b245200395
Hyunsu Park <jadest13x03@gmail.com>
2022-09-22 10:57:17 +0900

    네 번 출력

--- print.c
+++ print.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-int main(void)
-{
+int main(void) {
 	printf("GitHub\n");
 	printf("GitHub\n");
-	printf("GitHub\n");
+	printf("GitHub\n");//이미 세번입니다 교수님
+	printf("GitHub\n");//아하
 	return 0;
 }
